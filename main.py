b66a0aa420b06dc7c2b87257546372eacc541efe
abnuo <66645994+abnuo@users.noreply.github.com>
2021-08-25 19:10:49 -0700

    Update main.py

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ async def echo(websocket, path):
         username = await 'user' + str(binascii.crc32(websocket.remote_address[0].encode('utf-8')))
         await websocket.send(username + ': ' + str(message))
 
-start_server = websockets.serve(echo, "localhost", os.environ.get("PORT", 17995))
+start_server = websockets.serve(echo, "0.0.0.0", os.environ.get("PORT", 17995))
 
 asyncio.get_event_loop().run_until_complete(start_server)
 asyncio.get_event_loop().run_forever()
