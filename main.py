8e6e5c55a8172be755c6189f4b95af8229ea726e
abnuo <66645994+abnuo@users.noreply.github.com>
2021-08-25 20:02:26 -0700

    Update main.py

--- main.py
+++ main.py
@@ -4,9 +4,16 @@ import websockets
 import crc16
 
 async def echo(websocket, path):
-    async for message in websocket:
-        username = 'user' + str(crc16.crc16xmodem(websocket.remote_address[0].encode('utf-8')))
-        await websocket.send(username + ': ' + str(message))
+    # Register.
+    username = 'user' + str(crc16.crc16xmodem(websocket.remote_address[0].encode('utf-8')))
+    connected.add(websocket)
+    try:
+        async for message in websocket:
+            await asyncio.wait([ws.send(username + ': ' + str(message)) for ws in connected])
+            #await asyncio.sleep(10)
+    finally:
+        # Unregister.
+        connected.remove(websocket)
 
 start_server = websockets.serve(echo, "0.0.0.0", os.environ.get("PORT", 17995))
 
